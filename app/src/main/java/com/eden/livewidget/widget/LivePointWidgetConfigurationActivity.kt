732bfc8e729716cb5d7bf179d857801978c6572d
someone-s <workemaileden@gmail.com>
2025-09-08 17:03:34 +0100

    clean up imports

--- app/src/main/java/com/eden/livewidget/widget/LivePointWidgetConfigurationActivity.kt
+++ app/src/main/java/com/eden/livewidget/widget/LivePointWidgetConfigurationActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/eden/livewidget/widget/ui/ConfigurationBatteryPrompt.kt
+++ app/src/main/java/com/eden/livewidget/widget/ui/ConfigurationBatteryPrompt.kt
@@ -2,7 +2,6 @@ package com.eden.livewidget.widget.ui
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.provider.Settings
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,13 +11,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.core.net.toUri
 import com.eden.livewidget.R
 
 @Composable
