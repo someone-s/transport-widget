ff4607c028a1c96612b4001560b8e1de3c7f1095
someone-s <workemaileden@gmail.com>
2025-09-10 12:16:04 +0100

    mandate jdk17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,12 @@ room {
     schemaDirectory("$projectDir/schemas")
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
 android {
 
     namespace = "com.eden.livewidget"
@@ -38,11 +44,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
