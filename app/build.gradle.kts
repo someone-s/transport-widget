0cdb46403098f3d3a89bc59fde1d4e41ef18dec9
eden <workemaileden@gmail.com>
2026-08-09 15:54:38 +0100

    bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.eden.livewidget"
         minSdk = 24
         targetSdk = 37
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 20000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
